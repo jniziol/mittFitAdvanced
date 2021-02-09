375d0c5e6c13f3972fcf540ef767e6f2ed204eaf
Johnathan Niziol <35515+jniziol@users.noreply.github.com>
2021-02-09 10:01:20 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const form = document.querySelector('form');
 const activitiesTable = document.querySelector("section.activities table tbody");
 const activityHeader = document.querySelector('.activity-header');
+const formValidator = new Validator(form);
 
 form.onsubmit = function(e) {
-  e.preventDefault();
+  if (formValidator.validate()) {
+    const descriptionInput = document.querySelector("#description-input");
+    const timeInput = document.querySelector("#time-input");
+    const intensityInput = document.querySelector("#intensity-input");
+    activityTracker.addActivity(descriptionInput.value, parseInt(timeInput.value), parseInt(intensityInput.value));
+    
+    descriptionInput.value = "";
+    timeInput.value = "";
+    intensityInput.value = "";
+  }
 
-  const descriptionInput = document.querySelector("#description-input");
-  const timeInput = document.querySelector("#time-input");
-  const intensityInput = document.querySelector("#intensity-input");
-  const locationInput = document.querySelector("#location-input");
-  activityTracker.addActivity(descriptionInput.value, parseInt(timeInput.value), parseInt(intensityInput.value), locationInput.value);
-  
-  descriptionInput.value = "";
-  timeInput.value = "";
-  intensityInput.value = "";
-  locationInput.value = "";
   e.preventDefault();
 }
 
@@ -34,6 +34,7 @@ activityHeader.onclick = function(e) {
     const arrow =  "asc" === closestTH.dataset.sortOrder ? `<i class="las la-angle-up"></i>` : `<i class="las la-angle-down"></i>`
     closestTH.insertAdjacentHTML('beforeend', arrow);
     closestTH.dataset.sortOrder = "asc" === closestTH.dataset.sortOrder ? "desc" : "asc"
+    activityTracker.redrawTable();
   }
 }
 
@@ -51,11 +52,10 @@ function formatTime(minutes) {
 }
 
 class Activity {
-  constructor(description, time, intensity, location, person, id, date) {
+  constructor(description, time, intensity, person, id, date) {
     this.description = description;
     this.time = time;
     this.intensity = intensity;
-    this.location = location;
     this.id = id;
     this.person = person;
     this.date = date;
@@ -76,20 +76,38 @@ class ActivityTracker {
     this.person = person;
     this.activities = [];
     this.id = 0;
+
+    for(const activity of data) {
+      this.activities.push(
+        new Activity(
+          activity.description,
+          activity.time,
+          activity.intensity,
+          this.person,
+          this.id++,
+          activity.date,
+        )
+      );
+    };
+
+    this.redraw();
+    
   }
 
-  addActivity(description, time, intensity, location, date = new Date()) {
+  addActivity(description, time, intensity, date = new Date()) {
     this.activities.push(
       new Activity(
         description,
         time,
         intensity,
-        location,
         this.person,
         this.id++,
         date,
       )
     );
+
+    this.sortBy('date', 'desc');
+    updateChartData();
     this.redraw();
   }
 
@@ -123,7 +141,6 @@ class ActivityTracker {
   }
 
   updateheader() {
-
     const activitiesTotal = document.querySelector("#activities-total h3");
     const timeTotal = document.querySelector("#time-total h3");
     const averageCaloriesTotal = document.querySelector("#average-calories-total h3");
@@ -135,7 +152,7 @@ class ActivityTracker {
     caloriesTotal.textContent = this.totalCalories();
   }
 
-  sortBy(field, direction) {
+  sortBy(field, direction = 'asc') {
     this.activities.sort((a, b) => {
       if (a[field] !== '' && b[field] !== '' && !isNaN(a[field]) && !isNaN(b[field])) {
         return direction === 'asc' ? a[field] - b[field] :  b[field] - a[field];
@@ -143,7 +160,8 @@ class ActivityTracker {
         return direction === 'asc' ? a[field].toString().localeCompare(b[field]) : b[field].toString().localeCompare(a[field]);
       }
     });
-    this.redrawTable();
+
+    return this;
   }
 
   redrawTable() {
@@ -153,7 +171,6 @@ class ActivityTracker {
       activitiesTable.insertAdjacentHTML("beforeEnd",`
         <tr class="activity" data-id="${activity.id}">
           <td class="description">${activity.description}</td>
-          <td class="location">${activity.location}</td>
           <td class="calories">${Math.round(activity.calories)}</td>
           <td class="time">${activity.formattedTime()}</td>
           <td class="date">${activity.formattedDate()}</td>
@@ -176,24 +193,19 @@ class Person {
 const john = new Person("John", 86, 190, 40);
 const activityTracker = new ActivityTracker(john);
 
-data.forEach(function(activity) {
-  activityTracker.addActivity(
-    activity.description, 
-    activity.time,
-    activity.intensity,
-    activity.location,
-    activity.date,
-  )
-});
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+
+
+const recentActivities = activityTracker.sortBy('date', 'asc').activities.slice(activityTracker.activities.length - 5, activityTracker.activities.length)
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+        labels: recentActivities.map(activity => activity.description),
+        datasets: [{    
+            data: recentActivities.map(activity =>  Math.round(activity.calories)),
+            label: "Calories Burned",
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -210,19 +222,31 @@ var myChart = new Chart(ctx, {
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 2
         }]
     },
     options: {
-        scales: {
-            xAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        },
-        maintainAspectRatio: false,
-        
+      title: {
+        display: true,
+        fontSize: 20,
+        text: "Most Recent Activities"
+      },       
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      },
+      maintainAspectRatio: false,
     }
 });
 
+function updateChartData() {
+  const recentActivities = activityTracker.activities.slice(0, 5);
+
+  myChart.data.labels = recentActivities.map(activity => activity.description);
+  myChart.data.datasets.data = recentActivities.map(activity => Math.round(activity.calories));
+
+  myChart.update();
+}
